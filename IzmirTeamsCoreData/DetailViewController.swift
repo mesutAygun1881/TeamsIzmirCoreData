898171d78cc53213718be9d8a028710c58f48077
mesut aygun <mesutaygun35@gmail.com>
2021-05-28 00:12:31 +0300

    initial

--- IzmirTeamsCoreData/DetailViewController.swift
+++ IzmirTeamsCoreData/DetailViewController.swift
@@ -6,24 +6,104 @@
 //
 
 import UIKit
+import CoreData
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController ,UINavigationControllerDelegate , UIImagePickerControllerDelegate {
 
+    @IBOutlet var explainLabel: UITextView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var yearText: UITextField!
+    @IBOutlet var nameText: UITextField!
+    var chosenTeam = ""
+    var chosenTeamId : UUID?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red
+        imageView.isUserInteractionEnabled = true
+        let imageRecognizer = UITapGestureRecognizer(target: self, action: #selector(selectImage))
+        imageView.addGestureRecognizer(imageRecognizer)
+        
+        if chosenTeam != "" {
+            //coreData
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+             let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Teams")
+            let idString = chosenTeamId?.uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@", idString!)
+            fetchRequest.returnsObjectsAsFaults = false
+            do{
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+                        if let explain = result.value(forKey: "explain") as? String {
+                            explainLabel.text = explain
+                        }
+                        if let year = result.value(forKey: "year") as? String {
+                            yearText.text = String(year)
+                        }
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            imageView.image = image
+                            
+                        }
+                    }
+                }
+            }catch{
+                print("error")
+            }
+            
+        }else{
+            nameText.text = ""
+            yearText.text = ""
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func selectImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true)
+        
     }
-    */
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
 
+    }
+
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newTeam = NSEntityDescription.insertNewObject(forEntityName: "Teams", into: context)
+        
+        newTeam.setValue(nameText.text, forKey: "name")
+        newTeam.setValue(explainLabel.text, forKey: "explain")
+        
+        if let year = Int(yearText.text!) {
+            newTeam.setValue(year, forKey: "year")
+        }
+        newTeam.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        
+        newTeam.setValue(data, forKey: "image")
+        
+        do{
+            try context.save()
+            print("success")
+            
+        }catch{
+            print("error")
+        }
+        
+        NotificationCenter.default.post(name: NSNotification.Name("newData") , object: nil)
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
--- IzmirTeamsCoreData/ViewController.swift
+++ IzmirTeamsCoreData/ViewController.swift
@@ -6,14 +6,124 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+    
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    var selectedTeam = ""
+    var selectedId : UUID?
 
+    @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        getData()
+        
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "newData"), object: nil)
+    }
+    @objc func addButtonClicked(){
+        selectedTeam = ""
+        performSegue(withIdentifier: "toDetailVC", sender: nil)
+    }
+   
+    
+    @objc func getData() {
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Teams")
+        
+        fetchRequest.returnsObjectsAsFaults = false
+        do{
+            
+     let results = try  context.fetch(fetchRequest)
+           
+            for result in results as! [NSManagedObject] {
+                if let name = result.value(forKey: "name") as? String {
+                    self.nameArray.append(name)
+                     
+                }
+                if let id = result.value(forKey: "id") as? UUID {
+                    self.idArray.append(id)
+                     
+                }
+                self.tableView.reloadData()
+            }
+            
+        }catch{
+            print("error")
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedTeam = nameArray[indexPath.row]
+        selectedId = idArray[indexPath.row]
+        performSegue(withIdentifier: "toDetailVC", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailVC" {
+            let destinationVC = segue.destination as! DetailViewController
+            destinationVC.chosenTeam = selectedTeam
+            destinationVC.chosenTeamId = selectedId
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchedRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Teams")
+        let idString = idArray[indexPath.row].uuidString
+        fetchedRequest.predicate = NSPredicate(format: "id = %@", idString)
+        fetchedRequest.returnsObjectsAsFaults = false
+        
+        do{
+            
+            let results = try context.fetch(fetchedRequest)
+            
+            for result in results as! [NSManagedObject] {
+                if let id = result.value(forKey: "id") as? UUID {
+                    if id == idArray[indexPath.row] {
+                    context.delete(result)
+                    nameArray.remove(at: indexPath.row)
+                    idArray.remove(at: indexPath.row)
+                    self.tableView.reloadData()
+                    do{
+                        try context.save()
+                    }catch{
+                        
+                    }
+                    break
+                }
+            }
+            }
+            
+        }catch{
+            
+        }
+    }
+    
 }
 
